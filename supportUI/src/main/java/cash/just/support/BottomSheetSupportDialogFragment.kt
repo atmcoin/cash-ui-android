1063ef9a3b0a5f141be3c3920d6e9c9d64691020
dabitdev <imdavidfernandez@gmail.com>
2020-06-30 17:31:39 -0400

    renaming layout form fragment_support to fragment_support_page to avoid clashes with other apps / libraries

--- supportUI/src/main/java/cash/just/support/BottomSheetSupportDialogFragment.kt
+++ supportUI/src/main/java/cash/just/support/BottomSheetSupportDialogFragment.kt
@@ -23,7 +23,7 @@ class BottomSheetSupportDialogFragment : BottomSheetDialogFragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val view = inflater.inflate(R.layout.fragment_support, container, false)
+        val view = inflater.inflate(R.layout.fragment_support_page, container, false)
 
         arguments?.let {
             val title = it.getString(ARG_TITLE)
