3ac922d163e5ff4f12267f0f95cd1daaf4ce9810
dabitdev <imdavidfernandez@gmail.com>
2020-08-10 08:14:09 -0400

    rolling back fastadapter to 4.1.2

--- atmUI/src/main/java/cash/just/atm/AtmItem.kt
+++ atmUI/src/main/java/cash/just/atm/AtmItem.kt
@@ -33,7 +33,7 @@ open class AtmItem(var atmMachine: AtmMachine) : AbstractItem<AtmItem.ViewHolder
             description.text = null
         }
 
-        override fun bindView(item: AtmItem, payloads: List<Any>) {
+        override fun bindView(item: AtmItem, payloads: MutableList<Any>) {
             name.text = item.atmMachine.addressDesc
             description.text = "${item.atmMachine.city} ${item.atmMachine.zip}"
 
