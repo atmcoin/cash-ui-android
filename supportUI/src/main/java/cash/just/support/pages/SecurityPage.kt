3875210379f75769885048e6180b3d8c5735f7bb
dabitdev <imdavidfernandez@gmail.com>
2020-09-28 16:55:33 -0400

    adding ATM specific pages

--- supportUI/src/main/java/cash/just/support/pages/SecurityPage.kt
+++ supportUI/src/main/java/cash/just/support/pages/SecurityPage.kt
@@ -7,15 +7,11 @@ enum class SecurityPage(private val title: String, private val description: Stri
     BaseSupportPage {
     WHY_WRITE_DOWN_RECOVERY(
         "Why do I need to write down my recovery key?",
-        "The Coinsquare Wallet App connects directly to the cryptocurrency networks, where your wallets are stored. The \"keys\" to access your wallets are securely stored in your phone and nowhere else. The Coinsquare Wallet app will provide you with a unique \"Recovery Key,\" which is a list of 12-words which can be used to recreate the keys to your wallet. If you ever lose or upgrade your phone, this \"Recovery Key\" is required to regain access to your funds.\n" +
-                "\n" +
-                "Please write down each word, in order, on a piece of paper. We recommend storing your Recovery Key in a safe, safety deposit box at a bank, or wherever you might keep passports, birth certificates, or other important documents. Anyone who has your Recovery Key can access your money, even if they don't have your wallet PIN, so keep it private and don't show it to others! "
+        "The Coinsquare Wallet App connects directly to the cryptocurrency networks, where your wallets are stored. The \"keys\" to access your wallets are securely stored in your phone and nowhere else. The Coinsquare Wallet app will provide you with a unique \"Recovery Key,\" which is a list of 12-words which can be used to recreate the keys to your wallet. If you ever lose or upgrade your phone, this \"Recovery Key\" is required to regain access to your funds.\n\nPlease write down each word, in order, on a piece of paper. We recommend storing your Recovery Key in a safe, safety deposit box at a bank, or wherever you might keep passports, birth certificates, or other important documents. Anyone who has your Recovery Key can access your money, even if they don't have your wallet PIN, so keep it private and don't show it to others!"
     ),
     HOW_TO_RESET(
         "How do I reset my PIN with my recovery key?",
-        "Every time you create a new wallet with Coinsquare, you are provided with a \"Recovery Key,\" which is a list of 12 words that can be used to restore access to your wallet if you ever lose or upgrade your phone. You can also use your Recovery Key to reset your wallet PIN if you forget it.\n" +
-                "\n" +
-                "If you have incorrectly entered your PIN several times, you will be able to reset your PIN using your recovery key. Enter the 12 words from your Recovery Key in the correct order, and you will be able to choose a new PIN."
+        "Every time you create a new wallet with Coinsquare, you are provided with a \"Recovery Key,\" which is a list of 12 words that can be used to restore access to your wallet if you ever lose or upgrade your phone. You can also use your Recovery Key to reset your wallet PIN if you forget it.\n\nIf you have incorrectly entered your PIN several times, you will be able to reset your PIN using your recovery key. Enter the 12 words from your Recovery Key in the correct order, and you will be able to choose a new PIN."
     ),
     HOW_TO_CONFIRM(
         "How do I confirm my recovery key?",
@@ -23,9 +19,7 @@ enum class SecurityPage(private val title: String, private val description: Stri
     ),
     FINGERPRINT(
         "What is fingerprint authentication?",
-        "Many Android devices feature a fingerprint reader. You can use your fingerprint to unlock your Coinsquare wallet and authorize transactions instead of entering your passcode. You will still be required to enter your passcode periodically for increased security.\n" +
-                "\n" +
-                "In order to use this feature in the Coinsquare Wallet app, fingerprint authorization must be enabled on your device. See the phone manufacturer's manual for more information."
+        "Many Android devices feature a fingerprint reader. You can use your fingerprint to unlock your Coinsquare wallet and authorize transactions instead of entering your passcode. You will still be required to enter your passcode periodically for increased security.\n\nIn order to use this feature in the Coinsquare Wallet app, fingerprint authorization must be enabled on your device. See the phone manufacturer's manual for more information."
     );
 
     override fun title(): String {
--- supportUI/src/main/java/cash/just/support/pages/SettingPage.kt
+++ supportUI/src/main/java/cash/just/support/pages/SettingPage.kt
@@ -7,63 +7,23 @@ enum class SettingPage(private val title: String, private val description: Strin
         BaseSupportPage {
         HOW_WIPE_WALLET(
                 "How do I wipe my wallet?",
-                "The Coinsquare Wallet App can only have one wallet active at a time. If you choose to start a new wallet or recover an existing wallet, the currently active wallet must be erased first.\n" +
-                        "\n" +
-                        "IMPORTANT: If you erase your wallet without writing down its Recovery Key, your money will be lost. To prevent this, you must first enter your Recovery Key for the currently active wallet to make sure you have it written down correctly.\n" +
-                        "\n" +
-                        "Follow these steps to wipe a wallet:\n" +
-                        "\n" +
-                        "\t\t\t 1 - Tap on Menu in the main screen.\n" +
-                        "\t\t\t 2 - Select Security Settings.\n" +
-                        "\t\t\t 3 - Tap on Unlink from this device and then on Next.\n" +
-                        "\t\t\t 4 - Enter the current wallet’s Recovery Key.\n" +
-                        "\t\t\t 5 - Tap on Wipe.\n" +
-                        "\n" +
-                        "The wallet will then be erased, and you can create a new wallet or recover a previous wallet. "
+                "The Coinsquare Wallet App can only have one wallet active at a time. If you choose to start a new wallet or recover an existing wallet, the currently active wallet must be erased first.\n\nIMPORTANT: If you erase your wallet without writing down its Recovery Key, your money will be lost. To prevent this, you must first enter your Recovery Key for the currently active wallet to make sure you have it written down correctly.\n\nFollow these steps to wipe a wallet:\n\n\t\t\t 1 - Tap on Menu in the main screen.\n\t\t\t 2 - Select Security Settings.\n\t\t\t 3 - Tap on Unlink from this device and then on Next.\n\t\t\t 4 - Enter the current wallet’s Recovery Key.\n\t\t\t 5 - Tap on Wipe.\n\nThe wallet will then be erased, and you can create a new wallet or recover a previous wallet. "
         ),
         HOW_RECOVER_WALLET(
                 "How do I recover a previous wallet I had?",
-                "When you launch the Coinsquare Wallet app for the first time, you will be given the option to create a new wallet or to recover a wallet. If you have a 12 word recovery key from a previous Coinsquare wallet that you would like to recover, follow these steps to recover your wallet:\n" +
-                        "\n" +
-                        "\t\t\t 1 - Select Restore Wallet.\n" +
-                        "\t\t\t 2 - Click Next and enter the 12 word Recovery Key in the correct order for the wallet you want to recover.\n" +
-                        "\t\t\t 3 - Set a PIN.\n" +
-                        "\t\t\t 4 - Confirm the PIN.\n" +
-                        "\t\t\t 5 - Leave the app open and on the screen to allow the wallets to sync to the blockchain.\n" +
-                        "\n" +
-                        "Initial syncing can take up to 45 minutes or more so please prepare to leave the app open with the screen on for an extended period of time.\n" +
-                        "\n" +
-                        "If you wish to recover a wallet using a recovery key but you already have an active wallet loaded in the Coinsquare Wallet app, you will need to follow the directions to wipe the wallet before beginning the process above. If the wallet you are wiping has funds, be sure to write down the 12 word recovery key for the existing wallet (which can be found in the Security Center) before loading the recovery key for the wallet you wish to recover. Visit this page for help on wiping a wallet. "
+                "When you launch the Coinsquare Wallet app for the first time, you will be given the option to create a new wallet or to recover a wallet. If you have a 12 word recovery key from a previous Coinsquare wallet that you would like to recover, follow these steps to recover your wallet:\n\n\t\t\t 1 - Select Restore Wallet.\n\t\t\t 2 - Click Next and enter the 12 word Recovery Key in the correct order for the wallet you want to recover.\n\t\t\t 3 - Set a PIN.\n\t\t\t 4 - Confirm the PIN.\n\t\t\t 5 - Leave the app open and on the screen to allow the wallets to sync to the blockchain.\n\nInitial syncing can take up to 45 minutes or more so please prepare to leave the app open with the screen on for an extended period of time.\nIf you wish to recover a wallet using a recovery key but you already have an active wallet loaded in the Coinsquare Wallet app, you will need to follow the directions to wipe the wallet before beginning the process above. If the wallet you are wiping has funds, be sure to write down the 12 word recovery key for the existing wallet (which can be found in the Security Center) before loading the recovery key for the wallet you wish to recover. Visit this page for help on wiping a wallet. "
         ),
         HOW_CHANGE_CURRENCY(
                 "How do I change the local currency displayed in my wallet?",
-                "Your Coinsquare wallet will show your cryptocurrency and its equivalent fiat value in your local currency by default. \n" +
-                        "\n" +
-                        "You can change this to show the value in a different local currency by following these steps:\n" +
-                        "\n" +
-                        "\t\t\t 1 - On the main screen, tap Menu.\n" +
-                        "\t\t\t 2 - Select Preferences and then Display Currency.\n" +
-                        "\t\t\t 3 - Choose the currency you would like to use from the list.\n"
+                "Your Coinsquare wallet will show your cryptocurrency and its equivalent fiat value in your local currency by default. \n\n You can change this to show the value in a different local currency by following these steps:\n\n\t\t\t 1 - On the main screen, tap Menu.\n\t\t\t 2 - Select Preferences and then Display Currency.\n\t\t\t 3 - Choose the currency you would like to use from the list.\n"
         ),
         HOW_BALANCE_WORKS(
                 "How does the Coinsquare Bitcoin wallet show my balance in my local currency?",
-                "Money kept in your Coinsquare Bitcoin wallet is always stored as bitcoin. The app uses an exchange rate to show you an estimate of how much your bitcoin is worth in your local currency. When you send a transaction and enter the amount to send in your local currency, the app uses the exchange rate to calculate how much bitcoin to send.\n" +
-                        "\n" +
-                        "It's possible people using other wallets are using different exchange rates, so the balance sent/received might be slight different than what other person sees.\n" +
-                        "\n" +
-                        "Since the exchange rate is always changing, the amount of money in your wallet as quoted in your local currency may change over time. However, your bitcoin balance will not change unless you send or receive bitcoin. "
+                "Money kept in your Coinsquare Bitcoin wallet is always stored as bitcoin. The app uses an exchange rate to show you an estimate of how much your bitcoin is worth in your local currency. When you send a transaction and enter the amount to send in your local currency, the app uses the exchange rate to calculate how much bitcoin to send.\n\nIt's possible people using other wallets are using different exchange rates, so the balance sent/received might be slight different than what other person sees.\n\nSince the exchange rate is always changing, the amount of money in your wallet as quoted in your local currency may change over time. However, your bitcoin balance will not change unless you send or receive bitcoin. "
         ),
         HOW_SELECT_NODE(
                 "How do I connect to a specific bitcoin node?",
-                "The BRD app connects to a random node on the bitcoin network when syncing your wallet. You can choose to connect to a specific node of your preference by following the steps below:\n" +
-                        "\n" +
-                        "\t\t\t 1 - On the main screen, tap on Menu.\n" +
-                        "\t\t\t 2 - Select Preferences and then Bitcoin Settings.\n" +
-                        "\t\t\t 3 - Choose Bitcoin Nodes\n" +
-                        "\t\t\t 4 - Tap Switch to Manual Mode\n" +
-                        "\t\t\t 5 - Enter the Node IP address in the window that pops up.  You may also enter a port, although this is optional.  Tap OK when done.\n" +
-                        "\n" +
-                        "Your wallet will now sync to the blockchain via your preferred node."
+                "The BRD app connects to a random node on the bitcoin network when syncing your wallet. You can choose to connect to a specific node of your preference by following the steps below:\n\t\t\t 2 - Select Preferences and then Bitcoin Settings.\n\t\t\t 3 - Choose Bitcoin Nodes\n\t\t\t 4 - Tap Switch to Manual Mode\n\t\t\t 5 - Enter the Node IP address in the window that pops up.  You may also enter a port, although this is optional.  Tap OK when done.\n\nYour wallet will now sync to the blockchain via your preferred node."
         );
 
         override fun title(): String {
