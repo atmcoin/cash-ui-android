9bb717ad59e336bfbbcc5a062324aef82ebc0f16
dabitdev <imdavidfernandez@gmail.com>
2021-01-12 17:35:18 -0300

    getting ready to release 3.4

--- supportUI/src/main/java/cash/just/support/SupportFooterHelper.kt
+++ supportUI/src/main/java/cash/just/support/SupportFooterHelper.kt
@@ -1,13 +1,20 @@
 package cash.just.support
 
+import android.content.pm.PackageInfo
 import android.view.View
 import android.widget.TextView
 import androidx.lifecycle.LifecycleOwner
 import timber.log.Timber
 
+
 object SupportFooterHelper {
     fun populate(view:View, lifecycleOwner: LifecycleOwner) {
-        textView(view, R.id.versionNumber).text = BuildConfig.VERSION_NAME
+        val context = view.context
+        val pInfo: PackageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
+        val version = pInfo.versionName //Version Name
+        val verCode = pInfo.versionCode //Version Code
+
+        textView(view, R.id.versionNumber).text = "$version ($verCode))"
 
         textView(view, R.id.privacyLink).setOnClickListener {
             Timber.d("Click on privacyLink")
