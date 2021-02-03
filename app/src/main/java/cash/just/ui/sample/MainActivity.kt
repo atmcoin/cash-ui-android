20bbee1e8c2f1adb438052b6b0df84ba4e744ad5
Sampath Narayanan <sampathnarayanan72@gmail.com>
2021-02-03 10:36:47 -0500

    Added firebase functions and messaging methods

--- app/src/main/java/cash/just/ui/sample/MainActivity.kt
+++ app/src/main/java/cash/just/ui/sample/MainActivity.kt
@@ -1,20 +1,30 @@
 package cash.just.ui.sample
 
 import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.widget.Button
+import android.provider.Settings
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import cash.just.atm.base.AtmResult
 import cash.just.atm.model.BitcoinServer
 import cash.just.sdk.Cash
-import cash.just.support.*
-import cash.just.support.pages.SupportPagesLoader
-import cash.just.support.pages.Topic
+import cash.just.support.context
 import cash.just.ui.CashUI
-import com.google.gson.Gson
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.FirebaseFunctionsException
+import com.google.firebase.functions.ktx.functions
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -22,6 +32,8 @@ class MainActivity : AppCompatActivity() {
         private const val REQUEST_CODE_LIST = 0x02
         private const val REQUEST_CODE_STATUS = 0x03
     }
+
+    private lateinit var functions: FirebaseFunctions
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +50,10 @@ class MainActivity : AppCompatActivity() {
                 BitcoinServer.setServer(Cash.BtcNetwork.MAIN_NET)
             }
         }
+
+        functions = Firebase.functions
+
+        getToken()
 
         openMaps.setOnClickListener {
             CashUI.startCashOutActivityForResult(this@MainActivity, REQUEST_CODE_MAP)
@@ -82,6 +98,81 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
+        }
+    }
+
+    private fun getToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Timber.tag(TAG).w(task.exception, "Fetching FCM registration token failed")
+                return@OnCompleteListener
+            }
+            // Get new FCM registration token
+            val token = task.result
+
+            registerToken(token)
+                .addOnCompleteListener(OnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        val e = task.exception
+                        if (e is FirebaseFunctionsException) {
+                            val code = e.code
+                            val details = e.details
+                            Timber.tag(TAG).d("${code}:  $details")
+                        }
+                    } else {
+                        Timber.tag(TAG).d("Task successfull")
+                    }
+                })
+
+            Timber.tag(TAG).d(token)
+        })
+    }
+
+    private fun registerToken(token: String?): Task<String>  {
+        val packageVersion = getPackageVersion()
+        val createdAt = Calendar.getInstance().timeInMillis
+        val data = hashMapOf(
+            "fcmToken" to token,
+            "deviceId" to Settings.Secure.getString(
+                contentResolver,
+                Settings.Secure.ANDROID_ID
+            ),
+            "phone" to "",
+            "deviceModel" to Build.MODEL,
+            "appVersion" to packageVersion,
+            "createdAt" to createdAt
+        )
+        return functions
+            .getHttpsCallable("registerToken")
+            .call(data)
+            .continueWith { task ->
+                // This continuation runs on either success or failure, but if the task
+                // has failed then result will throw an Exception which will be
+                // propagated down.
+                val result = task.result?.data as String
+                result
+            }.addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Timber.tag(TAG).d("task unsuccessful: ${task.exception}")
+                    val e = task.exception
+                    if (e is FirebaseFunctionsException) {
+                        val code = e.code
+                        val details = e.details
+                        Timber.tag(TAG).d("${code}: $details")
+                    }
+                } else {
+                    Timber.tag(TAG).d("Task successfull")
+                }
+            })
+    }
+
+    private fun getPackageVersion(): Any {
+        return try {
+            val pInfo =
+                context.packageManager.getPackageInfo(context.packageName, 0)
+            pInfo.versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
         }
     }
 }
