c0e0660a78fa806e6f65ce289c6cf0e63744df2d
bytebug <david@nordicloop.com>
2021-03-01 14:52:01 -0500

    making firebase optional

---
+++ atmUI/src/main/java/cash/just/atm/base/FirebaseExtension.kt
@@ -0,0 +1,15 @@
+package cash.just.atm.base
+
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.ktx.functions
+import com.google.firebase.ktx.Firebase
+import timber.log.Timber
+
+fun Firebase.saveFunctions() : FirebaseFunctions? {
+    try {
+        Firebase.functions
+    } catch (exception : IllegalStateException) {
+        Timber.e("Firebase not initialized")
+    }
+    return null
+}
