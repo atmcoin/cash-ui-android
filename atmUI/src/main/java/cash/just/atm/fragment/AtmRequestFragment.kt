95515e25f9bcfffe209b34b43af236ddded2a106
bytebug <david@nordicloop.com>
2021-03-01 14:58:52 -0500

    fixing input number to text

--- atmUI/src/main/java/cash/just/atm/fragment/AtmRequestFragment.kt
+++ atmUI/src/main/java/cash/just/atm/fragment/AtmRequestFragment.kt
@@ -29,7 +29,6 @@ import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.functions.FirebaseFunctions
 import com.google.firebase.functions.FirebaseFunctionsException
-import com.google.firebase.functions.ktx.functions
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
 import com.square.project.base.singleStateObserve
